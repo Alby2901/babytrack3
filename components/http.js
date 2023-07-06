5f254caa75d331c48c786ecec343559654bc770a
Alby2901 <a.azzola@scriptanet.it>
2023-07-07 00:04:43 +0200

    Login Screen - Http Error

---
+++ components/http.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
+
+// const BASE_URL = "http://pinocomo.ddns.net:9999/babysafe/login?user=pino&pwd=pino";
+const BASE_URL = "http://pinocomo.ddns.net:9999/babysafe/login";
+
+export async function getSession(utente, password) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        user: utente,
+        pwd: password,
+      },
+    });
+    console.log(response);
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log('Data: ', error.response.data);
+      console.log('Status: ', error.response.status);
+      console.log('Header: ', error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log('Request: ', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error: ', error.message);
+    }
+    console.log('Config: ', error.config);
+    console.log('JSON: ', error.toJSON());
+  };
+
+  //   console.log("-------------------------------------------------");
+
+  //   console.log("Risposta: " + JSON.stringify(response));
+
+  console.log("-------------------------------------------------");
+
+  console.log("Risposta: " + JSON.stringify(response.data));
+
+  console.log("-------------------------------------------------");
+
+  for (const param in response.data) {
+    console.log("param: " + param);
+  }
+
+  console.log("param: " + response.data.ret);
+  console.log("param: " + response.data.message);
+  console.log("param: " + response.data.params);
+
+  const message = response.data.message;
+  let sessionID = "";
+
+  if (response.data.ret === -1) {
+    console.log("Ret -1 ");
+    sessionID = "";
+  } else {
+    console.log("Ret 0 ");
+    sessionID = response.data.params.sessionId;
+  }
+
+  console.log("SessionID: " + sessionID);
+
+  return [sessionID, message];
+}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,105 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { getSession } from "../components/http";
+import { useState } from "react";
 
 function LoginScreen({ navigation }) {
+  const [errorHTTP, setErrorHTTP] = useState("");
+  const [sessionID, setSessionID] = useState("");
+  const [message, setmessage] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+  const [usr, setUsr] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  async function getSessionHandler() {
+    console.log("+++++++++++++++++++++++++++++");
+    console.log("GetSession1: ");
+
+    try {
+      const [sessionIDData, messageData] = await getSession(usr, pwd);
+      console.log("GetSession2: ");
+      setSessionID(sessionIDData);
+      console.log("GetSession3: ");
+      setmessage(messageData);
+      console.log("GetSession4: ");
+      setIsLogged(true);
+    } catch (error) {
+      console.log("Errore HTTP: ", error.message);
+      setErrorHTTP(error.message);
+    }
+  }
+
+  function resetIsLogged() {
+    setIsLogged(false);
+  }
+
+  function usrInputHandler(enteredUsr) {
+    setUsr(enteredUsr);
+  }
+
+  function pwdInputHandler(enteredPwd) {
+    setPwd(enteredPwd);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>This is the Login Screen</Text>
-      <Button title='Input Screen' onPress={() => navigation.navigate('Input')} />
-      <Button title='Scan Screen' onPress={() => navigation.navigate('Scan')} />
+    <View style={styles.containerOuter}>
+      <View style={styles.containerUp}>
+        <Text>This is the Login Screen</Text>
+        <Button
+          title="Input Screen"
+          onPress={() => navigation.navigate("Input")}
+        />
+        <Button
+          title="Scan Screen"
+          onPress={() => navigation.navigate("Scan")}
+        />
+      </View>
+      <View style={styles.containerDown}>
+        <TextInput
+          style={styles.inputText}
+          onChangeText={usrInputHandler}
+          value={usr}
+          placeholder="Utente"
+        />
+        <TextInput
+          style={styles.inputText}
+          onChangeText={pwdInputHandler}
+          value={pwd}
+          placeholder="Password"
+        />
+        <Text style={styles.text}>Test chiamate HTTP...</Text>
+        <Button
+          style={styles.button}
+          title="Chek utente"
+          onPress={getSessionHandler}
+        ></Button>
+        {/* <Text>The Session ID is: {sessionID}</Text> */}
+        {isLogged && <Text>The Message is: {message}</Text>}
+        {isLogged && sessionID && <Text>The Session ID is: {sessionID}</Text>}
+        {isLogged && !sessionID && (
+          <Text style={styles.textAlert} cd>
+            The Session ID is not avaiable!
+          </Text>
+        )}
+        {isLogged && (
+          <Button
+            style={styles.button}
+            title="Annulla... "
+            onPress={resetIsLogged}
+          ></Button>
+        )}
+        {errorHTTP && (
+          <Text style={styles.textAlert} cd>
+            Server is not responding!
+          </Text>
+        )}
+      </View>
     </View>
   );
 }
@@ -13,9 +107,50 @@ function LoginScreen({ navigation }) {
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  containerOuter: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    paddin: 40,
+    backgroundColor: "#0099ff",
+  },
+  containerUp: {
+    flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+    // paddin: 80,
+    marginTop: 150,
+    marginBottom: 20,
+    // backgroundColor: "#00ff00",
+  },
+  containerDown: {
+    flex: 1,
+    // alignItems: "center",
+    // justifyContent: "center",
+    // paddin: 20,
+    marginTop: 20,
+    marginBottom: 200,
+    // backgroundColor: "#ff0000",
+  },
+  text: {
+    margin: 5,
+  },
+  textAlert: {
+    margin: 5,
+    color: "#ff0000",
+  },
+  button: {
+    margin: 5,
+  },
+
+  inputText: {
+    borderWidth: 2,
+    borderColor: "#ff0000",
+    backgroundColor: 'white',
+    color: 'white',
+    textDecorationColor: 'white',
+    margin: 5,
+    padding: 4,
+    width: 100,
   },
 });
