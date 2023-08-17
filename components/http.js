08c54aa2dffc596f73d2d25448075a007244736a
Alby2901 <a.azzola@scriptanet.it>
2023-08-17 19:29:35 +0200

    Ver 0.3.3 Problem With Wrong baby Code!

--- components/http.js
+++ components/http.js
@@ -54,15 +54,30 @@ export async function getSession(utente, password, url) {
 
 // const CHK_PAR_URL = "http://130.0.151.40:8090/babysafe/checkband";
 
-export async function getChkBaby(url, sessionid, neonanto) {
+export async function getChkBaby(url, sessionid, neonato) {
+  console.log('--------------- HTTP Component - getChkBaby - START --------------------------------------');
+  console.log('--------------- HTTP Component - getChkBaby - axio.get START ---------------------');
 
-  const urlComplete = url + '/' + GlobalConstants.paths.PATH_APP + '/' + GlobalConstants.paths.PATH_VERIFY; 
-   const response = await axios.get(urlComplete, {
-      params: {
-        sessionid: sessionid,
-        child: neonanto,
-      },
-    });
+  console.log('url: ', url);
+  console.log('GlobalConstants.paths.PATH_APP: ', GlobalConstants.paths.PATH_APP);
+  console.log('GlobalConstants.paths.PATH_VERIFY: ', GlobalConstants.paths.PATH_VERIFY);
+  console.log('sessionid: ', sessionid);
+  console.log('neonato: ', neonato);
+
+
+  const urlComplete = url + '/' + GlobalConstants.paths.PATH_APP + '/' + GlobalConstants.paths.PATH_VERIFY;
+
+  console.log('urlComplete: ', urlComplete);
+
+  const response = await axios.get(urlComplete, {
+    params: {
+      sessionid: sessionid,
+      child: neonato,
+    },
+  });
+
+  console.log('--------------- HTTP Component - getChkBaby - axio.get - THE END ---------------------');
+  
 
   console.log("http3: -------------------------------------------------");
   console.log("http3: Risposta chk gen: " + JSON.stringify(response.data));
@@ -95,21 +110,23 @@ export async function getChkBaby(url, sessionid, neonanto) {
   console.log("http3: MOTHER_NAME: " + motherName);
 
   // {"ret":0,"message":"Braccialetto madre riconosciuto ","params":{"childName":"Neonato Bimbo","motherName":"Rossi Maria"}}
+  
+  console.log('--------------- HTTP Component - getChkBaby - THE END --------------------------------------');
 
-  return [ ret, message, childName, motherName];
+  return [ret, message, childName, motherName];
 }
 
 
 export async function getChkParent(url, sessionid, neoanto, genitore) {
 
-  const urlComplete = url + '/' + GlobalConstants.paths.PATH_APP + '/' + GlobalConstants.paths.PATH_CHECHBAND; 
-   const response = await axios.get(urlComplete, {
-      params: {
-        sessionid: sessionid,
-        child: neoanto,
-        parent: genitore,
-      },
-    });
+  const urlComplete = url + '/' + GlobalConstants.paths.PATH_APP + '/' + GlobalConstants.paths.PATH_CHECHBAND;
+  const response = await axios.get(urlComplete, {
+    params: {
+      sessionid: sessionid,
+      child: neoanto,
+      parent: genitore,
+    },
+  });
 
   console.log("http2: -------------------------------------------------");
   console.log("http2: Risposta chk gen: " + JSON.stringify(response.data));
@@ -143,5 +160,5 @@ export async function getChkParent(url, sessionid, neoanto, genitore) {
 
   // {"ret":0,"message":"Braccialetto madre riconosciuto ","params":{"childName":"Neonato Bimbo","motherName":"Rossi Maria"}}
 
-  return [ ret, message, childName, motherName];
+  return [ret, message, childName, motherName];
 }
